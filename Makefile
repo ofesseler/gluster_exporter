392ed0d57083e0cd033ce8ab246a9fbcd84b4e3e
Oliver Fesseler <ofesseler@gmail.com>
2016-11-21 15:33:42 +0100

    adds testing for public methods

--- Makefile
+++ Makefile
@@ -4,10 +4,12 @@ info:
 	@echo "build: Go build"
 	@echo "docker: build and run in docker container"
 
-build:
+build: gotest
 	$(GO) build -o gluster_exporter main.go
 
-docker: build
+docker: gotest build
 	docker build -t gluster-exporter-test .
 	docker run --rm --privileged=true -p 9189:9189 -p 24007:24007 -p 24008:24008 -ti -v gluster-test:/data gluster-exporter-test
 
+gotest:
+	$(GO) test -v .
---
+++ main_test.go
@@ -0,0 +1,24 @@
+package main
+
+
+import (
+	"testing"
+	"io/ioutil"
+	"bytes"
+)
+
+func TestInfoUnmarshall(t *testing.T) {
+	testXmlPath := "test/gluster_volume_info.xml"
+	t.Log("Test xml unmarshal for gluster volume info with file: ", testXmlPath)
+	dat, err := ioutil.ReadFile(testXmlPath)
+
+	if err != nil {
+		t.Errorf("error reading testxml in Path: %v", testXmlPath)
+	}
+
+	glusterVolumeInfo := infoUnmarshall(bytes.NewBuffer(dat))
+	if glusterVolumeInfo.OpErrno != 0 && glusterVolumeInfo.VolInfo.Volumes.Count == 2 {
+		t.Error("something wrong")
+	}
+	t.Log("gluster volume info test was sucessful.")
+}
