5eb7d08b4f71dbbe4244d315447915a73b10b073
Fabien Dugast <fabien.dugast@stormshield.eu>
2017-04-19 12:06:57 +0200

    Add new test for volume quota list

--- structs/xmlStructs_test.go
+++ structs/xmlStructs_test.go
@@ -226,3 +226,39 @@ func TestVolumeHealInfoXMLUnmarshall(t *testing.T) {
 	}
 
 }
+
+func TestVolumeQuotaListXMLUnmarshall(t *testing.T) {
+    testXMLPath := "../test/gluster_volume_quota_list.xml"
+    nodeCount := 2
+    dat, err := ioutil.ReadFile(testXMLPath)
+
+	if err != nil {
+		t.Errorf("error reading testxml in Path: %v", testXMLPath)
+	}
+	volumeQuotaXML, err := VolumeQuotaListXMLUnmarshall(bytes.NewBuffer(dat))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if volumeQuotaXML.OpErrno != 0 {
+		t.Error(volumeQuotaXML.OpErrstr)
+	}
+    nb_limits := len(volumeQuotaXML.VolQuota.QuotaLimits)
+    if nb_limits != nodeCount {
+        t.Error("Expected %v Limits and len is %v", nodeCount, nb_limits)
+    }
+
+    for _, limit := range volumeQuotaXML.VolQuota.QuotaLimits {
+        if limit.Path == "/foo" {
+            if limit.AvailSpace != 10309258240 {
+                t.Error(
+                    "Expected %v for available space in path %v, got %v",
+                    1811939328,
+                    limit.Path,
+                    limit.AvailSpace,
+                )
+            }
+        }
+    }
+
+}
